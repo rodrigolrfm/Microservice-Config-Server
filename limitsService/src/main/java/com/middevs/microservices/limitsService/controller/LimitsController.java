17136b36db149f125f5428080eb460f73ea40c29
rodrigolrfm <rfernandez@novatronic.com>
2023-07-25 00:15:55 -0500

    Begin:Developing microservices

---
+++ limitsService/src/main/java/com/middevs/microservices/limitsService/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.middevs.microservices.limitsService.controller;
+
+import com.middevs.microservices.limitsService.bean.Limits;
+import com.middevs.microservices.limitsService.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+
+}
---
+++ spring-cloud-config-server/src/test/java/com/middevs/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middevs.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
